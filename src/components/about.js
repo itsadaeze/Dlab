2a4a99326167e3b0caef072894003469f0b43f5f
itsadaeze <adaezeugwu6@gmail.com>
2024-01-23 11:57:36 +0100

    worked on responsiveness

--- src/components/about.js
+++ src/components/about.js
@@ -5,9 +5,9 @@ import backgroundImage from "./../images/about-bg.svg";
 const About = () => {
   return (
     <div
-      className="flex justify-center relative text-center w-full h-full mb-[3rem] "
+      className="flex justify-center relative text-center w-full h-[40vh] mb-0 md:mb-[1rem] md:h-full "
       style={{
-        height: "650px",
+        height: "550px",
       }}
     >
       <div
@@ -18,18 +18,18 @@ const About = () => {
         }}
       />
       <div className="relative z-10 w-full mt-[1rem]  md:w-[50%]">
-        <h1 className="font-orbitron text-[1rem]  md:text-[2.5rem]">
+        <h1 className="font-orbitron text-[1rem] px-5 md:text-[2.5rem] md:px-0">
           User - Centric, <span className="text-[#8D1DE5]">Seamless</span> Functionality, Empowering Business,
           <span className="text-[#8D1DE5]"> Innovative</span> Approach
         </h1>
 
     
-        <h3 className="text-[#333] font-montserrat text-left tracking-wider text-[12px] leading-relaxed mt-[2rem] px-[1rem] md:mt-[4rem] md:px-[2rem] md:text-sm" >
+        <h3 className="text-[#333] font-montserrat text-left tracking-wider text-[12px] leading-relaxed mt-[1rem] px-[1rem] md:mt-[2rem] md:px-[2rem] md:text-sm" >
           Welcome to our innovative design and development agency! We craft
           stunning visual identities and bring them to life with seamless
           functionality, ensuring your brand stands out in the digital world.
         </h3>
-        <h3 className="text-[#333] font-montserrat text-left tracking-wider text-[12px] leading-relaxed mt-[2rem] px-[1rem] md:mt-[4rem] md:px-[2rem] md:text-sm" >
+        <h3 className="text-[#333] font-montserrat text-left tracking-wider text-[12px] leading-relaxed mt-[1rem] px-[1rem] md:mt-[2rem] md:px-[2rem] md:text-sm" >
           We're a team of creative geniuses and tech wizards who have joined
           forces to bring you the most stunning websites and mind-blowing
           digital experiences. Our portfolio includes work for some of the
@@ -37,7 +37,7 @@ const About = () => {
           it. From sleek, minimalist designs to eye-popping animations, we've
           got the skills and the passion to make your digital dreams come true.
         </h3>
-        <h3 className="text-[#333] font-montserrat text-left tracking-wider text-[12px] leading-relaxed mt-[2rem] px-[1rem] md:mt-[4rem] md:px-[2rem] md:text-sm" >
+        <h3 className="text-[#333] font-montserrat text-left tracking-wider text-[12px] leading-relaxed mt-[1rem] px-[1rem] md:mt-[2rem] md:px-[2rem] md:text-sm" >
           So, buckle up and get ready for a wild ride into the world of
           cutting-edge design and development. Let's create something
           extraordinary together.
--- src/components/clientfeedback.js
+++ src/components/clientfeedback.js
@@ -32,7 +32,7 @@ const ClientFeedback = () => {
         
       ];
     return(
-        <div className="bg-[#F2F2F2] h-[210vh] md:h-[120vh]">
+        <div className="bg-[#F2F2F2] h-[200vh] md:h-[120vh]">
         <h1 className="text-[#333] text-[1.5rem] font-orbitron px-[1rem] py-[1rem] tracking-wide font-light md:text-[3rem] md:px-[5rem] md:py-[3rem]">Clients <span className="text-[#8D1DE5]">Feedback</span></h1>
 
         <div className="flex justify-center items-center overflow-auto ">
