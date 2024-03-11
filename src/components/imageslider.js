bbcd6730a122bf896425fcac1a80729d11f336be
itsadaeze <adaezeugwu6@gmail.com>
2024-03-11 14:04:17 +0100

    fixed the white space

--- src/components/imageslider.js
+++ src/components/imageslider.js
@@ -4,17 +4,17 @@ import React, { useEffect, useRef } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import slide1 from "./../images/carousel11.svg"
-import slide2 from "./../images/carousel2.svg"
-import slide3 from "./../images/carousel3.svg"
-import slide4 from "./../images/carousel4.svg"
-import slide5 from "./../images/carousel5.svg"
-import slide6 from "./../images/carousel6.svg"
-import slide7 from "./../images/carousel7.svg"
-import slide8 from "./../images/carousel11.svg"
+import slide1 from "./../images/IMG_20230307_124811_156.png"
+import slide2 from "./../images/IMG_20230628_171211_324.png"
+import slide3 from "./../images/IMG_20230628_171221_794.png"
+import slide4 from "./../images/IMG_20230701_163704_090.png"
+import slide5 from "./../images/IMG_20230803_073710_218.png"
+import slide6 from "./../images/IMG_20230805_150842_881.png"
+import slide7 from "./../images/IMG_20230925_113538_866.png"
+import slide8 from "./../images/IMG_20240115_214919_457.png"
 
 const images = [
-  slide1, slide2, 'https://s3-alpha-sig.figma.com/img/cc77/0215/a785ddcdd1d57f9e89d1c8ebbdf83caf?Expires=1706486400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=HU1Hk-RlSe6bPbgb58okTD2YtvQx0XMSYhv2Zn83pg4wGfjhHfoxbPjIBWIv2H6ZJFVkcZycu0CVAyWXXZG-wWaX0d8fM4cZpSl121-8uvEgYL1FBa6MWLlEyJP9bEPWDhmhbGX33v-4GDkHr0VRzjvBDKFgs9Ckx8SPVF3sLX-PaWdBn7DSy6YL7J3sCYqujFj9z4MMvN3SAIoMs4pGLgse12PITnyn~Ucj-Wx~N7wuLyKY2rJOqa4s2g2lBkFAVHJHTuiTG0mQkD7kxE6EhvtMNAXagBCpj8bNSm~vk6un7dF0EvNWWZYEXqu4vqNqMzVq6tMcTtgvo4WTKnj89Q__', slide3, slide4, "https://s3-alpha-sig.figma.com/img/0cf5/5790/edd8f3d8ce91b0d7f58c3a40d2c8fbef?Expires=1706486400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=KyZH83CpZLfXsTawG2RH8jfrZuJMpcHu6IQZpVrweg713PUfZfP~OsM4FLSu217YlNboCF4zblgoOI~TuRos3IxA~x6l1ilz5Sx85njfnwF7YrRNNYWa81WGBJR3-PAE5kWZEsOCKBIA8mY~-jcMC3YuW1fzyD9lTDF69heUPGv4dyaS-S-Kl7ec~zwtXD5ofNyLnY5r77tLN5ZPWAY3qsnI4MBq3mTmvbF1NzINmBHownuLyvqMUwrxtj6pkGRoiJAd1ENqDgYJaRtIM67DFyutDF~z1FRvM1Nd~VvfU1NyvFE6fzF3h0fZ9W1JTL9qtWWKPSpzRlLomk9xwzV1RA__", slide5, slide6, "https://s3-alpha-sig.figma.com/img/cc77/0215/a785ddcdd1d57f9e89d1c8ebbdf83caf?Expires=1706486400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=HU1Hk-RlSe6bPbgb58okTD2YtvQx0XMSYhv2Zn83pg4wGfjhHfoxbPjIBWIv2H6ZJFVkcZycu0CVAyWXXZG-wWaX0d8fM4cZpSl121-8uvEgYL1FBa6MWLlEyJP9bEPWDhmhbGX33v-4GDkHr0VRzjvBDKFgs9Ckx8SPVF3sLX-PaWdBn7DSy6YL7J3sCYqujFj9z4MMvN3SAIoMs4pGLgse12PITnyn~Ucj-Wx~N7wuLyKY2rJOqa4s2g2lBkFAVHJHTuiTG0mQkD7kxE6EhvtMNAXagBCpj8bNSm~vk6un7dF0EvNWWZYEXqu4vqNqMzVq6tMcTtgvo4WTKnj89Q__", slide7, slide8
+  slide1, slide2, 'https://s3-alpha-sig.figma.com/img/cc77/0215/a785ddcdd1d57f9e89d1c8ebbdf83caf?Expires=1706486400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=HU1Hk-RlSe6bPbgb58okTD2YtvQx0XMSYhv2Zn83pg4wGfjhHfoxbPjIBWIv2H6ZJFVkcZycu0CVAyWXXZG-wWaX0d8fM4cZpSl121-8uvEgYL1FBa6MWLlEyJP9bEPWDhmhbGX33v-4GDkHr0VRzjvBDKFgs9Ckx8SPVF3sLX-PaWdBn7DSy6YL7J3sCYqujFj9z4MMvN3SAIoMs4pGLgse12PITnyn~Ucj-Wx~N7wuLyKY2rJOqa4s2g2lBkFAVHJHTuiTG0mQkD7kxE6EhvtMNAXagBCpj8bNSm~vk6un7dF0EvNWWZYEXqu4vqNqMzVq6tMcTtgvo4WTKnj89Q__', slide3, slide4, slide5, slide6, "https://s3-alpha-sig.figma.com/img/cc77/0215/a785ddcdd1d57f9e89d1c8ebbdf83caf?Expires=1706486400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=HU1Hk-RlSe6bPbgb58okTD2YtvQx0XMSYhv2Zn83pg4wGfjhHfoxbPjIBWIv2H6ZJFVkcZycu0CVAyWXXZG-wWaX0d8fM4cZpSl121-8uvEgYL1FBa6MWLlEyJP9bEPWDhmhbGX33v-4GDkHr0VRzjvBDKFgs9Ckx8SPVF3sLX-PaWdBn7DSy6YL7J3sCYqujFj9z4MMvN3SAIoMs4pGLgse12PITnyn~Ucj-Wx~N7wuLyKY2rJOqa4s2g2lBkFAVHJHTuiTG0mQkD7kxE6EhvtMNAXagBCpj8bNSm~vk6un7dF0EvNWWZYEXqu4vqNqMzVq6tMcTtgvo4WTKnj89Q__", slide7, slide8
 ];
 
 const ImageSlider = () => {
